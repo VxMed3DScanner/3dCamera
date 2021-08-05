8c56d9b973986617732e03e04e1d3d27ee03f3fe
VxMed3DScanner <86808149+VxMed3DScanner@users.noreply.github.com>
2021-08-05 13:18:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-var version = '1.32';
+var version = '1.4';
 
 var args = process.argv.slice(2);
 
@@ -176,7 +176,7 @@ function sendImage(code) {
         });
     });
     
-    var fileName = photoStartTime.getSeconds() + photoStartTime.getMilliseconds() + '.jpg';
+    var fileName = photoStartTime + '.jpg';
     
     // Post the image data via an http request
     var form = new FormData();
